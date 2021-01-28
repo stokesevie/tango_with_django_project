60845c13e90dfc48357fd1b05c59f9ca579f7b13
Evie Stokes <2444030s@student.gla.ac.uk>
2021-01-28 20:09:59 +0000

    chp5 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -26,11 +26,11 @@ def populate():
          'url':'http://bottlepy.org/docs/dev/'},
          {'title':'Flask',
           'url':'http://flask.pocoo.org'}]
-    cats={'Python': {'pages': python_pages},
-          'Django': {'pages': django_pages},
-           'Other Frameworks': {'pages': other_pages} }
+    cats={'Python': {'pages': python_pages, 'likes': 64, 'views':128},
+          'Django': {'pages': django_pages, 'likes': 32, 'views':64},
+           'Other Frameworks': {'pages': other_pages,  'likes': 16, 'views':32}}
     for cat, cat_data in cats.items():
-       c = add_cat(cat)
+       c = add_cat(cat, cat_data['views'],cat_data['likes'])
        for p in cat_data['pages']:
            add_page(c, p['title'], p['url'])
     for c in Category.objects.all():
@@ -44,8 +44,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
     c.save()
     return c
 
