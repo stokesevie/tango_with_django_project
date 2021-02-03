77f2839fa5c50858ea42abee0b4f6683b5563319
Evie Stokes <2444030s@student.gla.ac.uk>
2021-02-03 14:22:10 +0000

    chp7 exercises +base.html

--- rango/forms.py
+++ rango/forms.py
@@ -12,22 +12,22 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name',)
 
-    class PageForm(forms.ModelForm):
-        title = forms.CharField(max_length=128,
-        help_text="Please enter the title of the page.")
-        url = forms.URLField(max_length=200,
-        help_text="Please enter the URL of the page.")
-        views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-        def clean(self):
-            cleaned_data = self.cleaned_data
-            url = cleaned_data.get('url')
-            # If url is not empty and doesn't start with 'http://',
-            # then prepend 'http://'.
-            if url and not url.startswith('http://'):
-                url = f'http://{url}'
-                cleaned_data['url'] = url
-                return cleaned_data
-class Meta:
-    model = Page
-    exclude = ('category',)
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128,
+    help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200,
+    help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+            return cleaned_data
+    class Meta:
+        model = Page
+        exclude = ('category',)
 
